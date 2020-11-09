65a58380a7e44a584c4113f748206f6d7a504217
leave20 <thetai6102@gmail.com>
2020-11-09 09:17:54 -0500

    first workshop : size - devices and images

--- app/src/main/java/com/example/tallerandroid1/MainActivity.java
+++ app/src/main/java/com/example/tallerandroid1/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.tallerandroid1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+//TextView textView;
+//Button button;
+//EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        textView=findViewById(R.id.textView);
+//        button=findViewById(R.id.button);
+//        editText=findViewById(R.id.editText);
+//
+//        button.setOnClickListener(v -> {
+//            String texto=editText.getText().toString();
+//            textView.setText(texto);
+//
+//        });
     }
 }
